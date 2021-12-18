49ee7d99c1a6a2016857dff9baaa93ebdd271779
tigerfsh <mrsmish@yahoo.com>
2021-12-18 13:53:19 +0800

    Separate write with read successfuly

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy, SignallingSession, get_state
 from sqlalchemy import orm
 from sqlalchemy import func 
+from sqlalchemy.sql.selectable import Select
+from sqlalchemy.sql.dml import Update
+from sqlalchemy.sql.dml import Delete
+
 from contextlib import contextmanager
 
 app = Flask(__name__)
@@ -21,13 +25,15 @@ app.config['SQLALCHEMY_BINDS'] = {  # get_engine的bind参数为该配置的键
 class RoutingSession(SignallingSession):
     def get_bind(self, mapper=None, clause=None):
         """当进行数据操作时, 会调用该方法来获取进行该操作的数据库引擎(连接)"""
-        print("#"*50)
-        print(f"map: {mapper}")
-        print(f"clause: {clause}, {type(clause)}")
-        print("#"*50)
+        # print("#"*50)
+        # print(f"map: {mapper}")
+        # print(f"clause: {clause}, {type(clause)}")
+        # print(f"_flushing: {self._flushing}")
+        # print("#"*50)
 
         state = get_state(self.app)
-        if self._flushing:  # 增删改操作, 使用主库
+        if self._flushing or isinstance(clause, (Update, Delete)):
+        # if self._flushing:  # 增删改操作, 使用主库
             print('使用主库')
             return state.db.get_engine(self.app, bind='master')
         else:  # 读操作, 使用从库
@@ -61,13 +67,13 @@ class User(db.Model):
     age = db.Column(db.Integer, default=10, index=True)  # 设置默认值约束&建立索引
 
 
-@app.route('/')
+@app.route('/add', methods=["POST"])
 def index():  
     # 增加数据  进行检验是否读写分离
     # 1.创建模型对象
-    print(f"{request.args}")
-    name = request.args.get("name")
-    age = request.args.get("age", 18)
+    print(f"{request.json}")
+    name = request.json.get("name")
+    age = request.json.get("age", 18)
     print(f"{name}: {age}")
 
     with db.auto_commit():
@@ -78,6 +84,23 @@ def index():
     # 查询数据  
     print(User.query.all())
     return jsonify(success=True)
+
+@app.route("/update", methods=["POST"])
+def update():
+    id = request.json.get("id")
+    age = request.json.get("age", 18)
+    v = request.json.get("v", 1)
+    if v == 1:
+        if id:
+            with db.auto_commit():
+                User.query.filter_by(id=id).update({"age": age})
+    elif v == 2:
+        user = User.query.filter_by(id=id).first()
+        with db.auto_commit():
+            user.age = age 
+            db.session.add(user)
+
+    return jsonify()
 
 @app.route("/del")
 def del_obj():
@@ -100,9 +123,12 @@ def del_obj():
 @app.route("/list")
 def get_all_users():
     # count() vs func.count()
-    print(User.query.count())
-    print(db.session().query(func.count(User.id)).scalar())
-    return jsonify()
+    # print(User.query.count())
+    # print(db.session().query(func.count(User.id)).scalar())
+
+    users = User.query.all()
+    data = [{"id": user.id, "name": user.name, "age": user.age} for user in users]
+    return jsonify(data=data)
 
 
 if __name__ == '__main__':
